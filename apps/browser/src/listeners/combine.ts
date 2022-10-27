bcfc836e90f10a3b72fabd2222ea512cc48f198d
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2022-10-27 13:46:58 -0400

    Switch to using new cache per run

--- apps/browser/src/listeners/combine.ts
+++ apps/browser/src/listeners/combine.ts
@@ -4,9 +4,10 @@ type Listener<T extends unknown[]> = (...args: [...T, CachedServices]) => void;
 
 export const combine = <T extends unknown[]>(
   listeners: Listener<T>[],
-  cachedServices: CachedServices = {}
+  startingServices: CachedServices = {}
 ) => {
   return (...args: T) => {
+    const cachedServices = { ...startingServices };
     for (const listener of listeners) {
       listener(...[...args, cachedServices]);
     }

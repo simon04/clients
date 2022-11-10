beastycoding-forks/bitwarden-clients

Move importer-related files into libs/importers

libs/importers/spec/importers/onepassword-win-csv-importer.spec.ts
@@ -1,0 +1,87 @@
+import { CipherType } from "@bitwarden/common/enums/cipherType";
+import { FieldType } from "@bitwarden/common/enums/fieldType";
+import { CipherView } from "@bitwarden/common/models/view/cipher.view";
+import { FieldView } from "@bitwarden/common/models/view/field.view";
+import { OnePasswordWinCsvImporter as Importer } from "@bitwarden/importers/importers/onepasswordImporters/onepassword-win-csv-importer";
+
+import { data as creditCardData } from "./test-data/onepassword-csv/credit-card.windows.csv";
+import { data as identityData } from "./test-data/onepassword-csv/identity.windows.csv";
+import { data as multiTypeData } from "./test-data/onepassword-csv/multiple-items.windows.csv";
+
+function expectIdentity(cipher: CipherView) {
+  expect(cipher.type).toBe(CipherType.Identity);
+
+  expect(cipher.identity).toEqual(
+    expect.objectContaining({
+      firstName: "first name",
+      middleName: "mi",
+      lastName: "last name",
+      username: "userNam3",
+      company: "bitwarden",
+      phone: "8005555555",
+      email: "email@bitwarden.com",
+    })
+  );
+
+  expect(cipher.fields).toEqual(
+    expect.arrayContaining([
+      Object.assign(new FieldView(), {
+        type: FieldType.Text,
+        name: "address",
+        value: "address city state zip us",
+      }),
+    ])
+  );
+}
+
+function expectCreditCard(cipher: CipherView) {
+  expect(cipher.type).toBe(CipherType.Card);
+
+  expect(cipher.card).toEqual(
+    expect.objectContaining({
+      number: "4111111111111111",
+      code: "111",
+      cardholderName: "test",
+      expMonth: "1",
+      expYear: "1970",
+    })
+  );
+}
+
+describe("1Password windows CSV Importer", () => {
+  let importer: Importer;
+  beforeEach(() => {
+    importer = new Importer();
+  });
+
+  it("should parse identity records", async () => {
+    const result = await importer.parse(identityData);
+
+    expect(result).not.toBeNull();
+    expect(result.success).toBe(true);
+    expect(result.ciphers.length).toBe(1);
+    const cipher = result.ciphers[0];
+    expectIdentity(cipher);
+  });
+
+  it("should parse credit card records", async () => {
+    const result = await importer.parse(creditCardData);
+
+    expect(result).not.toBeNull();
+    expect(result.success).toBe(true);
+    expect(result.ciphers.length).toBe(1);
+    const cipher = result.ciphers[0];
+    expectCreditCard(cipher);
+  });
+
+  it("should parse csv's with multiple record types", async () => {
+    const result = await importer.parse(multiTypeData);
+
+    expect(result).not.toBeNull();
+    expect(result.success).toBe(true);
+    expect(result.ciphers.length).toBe(4);
+
+    expectIdentity(result.ciphers[1]);
+    expectCreditCard(result.ciphers[2]);
+  });
+});

libs/importers/spec/importers/test-data/onepassword-1pux/email-field-on-identity_prefilled.ts
@@ -1,0 +1,103 @@
+import { ExportData } from "@bitwarden/importers/importers/onepasswordImporters/types/onepassword-1pux-importer-types";
+
+export const EmailFieldOnIdentityPrefilledData: ExportData = {
+  accounts: [
+    {
+      attrs: {
+        accountName: "1Password Customer",
+        name: "1Password Customer",
+        avatar: "",
+        email: "username123123123@gmail.com",
+        uuid: "TRIZ3XV4JJFRXJ3BARILLTUA6E",
+        domain: "https://my.1password.com/",
+      },
+      vaults: [
+        {
+          attrs: {
+            uuid: "pqcgbqjxr4tng2hsqt5ffrgwju",
+            desc: "Just test entries",
+            avatar: "ke7i5rxnjrh3tj6uesstcosspu.png",
+            name: "T's Test Vault",
+            type: "U",
+          },
+          items: [
+            {
+              uuid: "45mjttbbq3owgij2uis55pfrlq",
+              favIndex: 0,
+              createdAt: 1619465450,
+              updatedAt: 1619465789,
+              trashed: false,
+              categoryUuid: "004",
+              details: {
+                loginFields: [],
+                notesPlain: "",
+                sections: [
+                  {
+                    title: "Identification",
+                    name: "name",
+                    fields: [],
+                  },
+                  {
+                    title: "Address",
+                    name: "address",
+                    fields: [],
+                  },
+                  {
+                    title: "Internet Details",
+                    name: "internet",
+                    fields: [
+                      {
+                        title: "email",
+                        id: "email",
+                        value: {
+                          string: "gengels@nullvalue.test",
+                        },
+                        indexAtSource: 4,
+                        guarded: false,
+                        multiline: false,
+                        dontGenerate: false,
+                        inputTraits: {
+                          keyboard: "emailAddress",
+                          correction: "default",
+                          capitalization: "default",
+                        },
+                      },
+                      {
+                        title: "2nd email",
+                        id: "2nd_email",
+                        value: {
+                          email: {
+                            email_address: "kriddler@nullvalue.test",
+                            provider: "myEmailProvider",
+                          },
+                        },
+                        indexAtSource: 1,
+                        guarded: false,
+                        multiline: false,
+                        dontGenerate: false,
+                        inputTraits: {
+                          keyboard: "emailAddress",
+                          correction: "default",
+                          capitalization: "default",
+                        },
+                      },
+                    ],
+                  },
+                ],
+                passwordHistory: [],
+              },
+              overview: {
+                subtitle: "George Engels",
+                title: "George Engels",
+                url: "",
+                ps: 0,
+                pbe: 0.0,
+                pgrng: false,
+              },
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};

libs/importers/spec/importers/test-data/onepassword-1pux/rewards-program.ts
@@ -1,0 +1,209 @@
+import { ExportData } from "@bitwarden/importers/importers/onepasswordImporters/types/onepassword-1pux-importer-types";
+
+export const RewardsProgramData: ExportData = {
+  accounts: [
+    {
+      attrs: {
+        accountName: "1Password Customer",
+        name: "1Password Customer",
+        avatar: "",
+        email: "username123123123@gmail.com",
+        uuid: "TRIZ3XV4JJFRXJ3BARILLTUA6E",
+        domain: "https://my.1password.com/",
+      },
+      vaults: [
+        {
+          attrs: {
+            uuid: "pqcgbqjxr4tng2hsqt5ffrgwju",
+            desc: "Just test entries",
+            avatar: "ke7i5rxnjrh3tj6uesstcosspu.png",
+            name: "T's Test Vault",
+            type: "U",
+          },
+          items: [
+            {
+              uuid: "3bmrdcml3tngvsr6zdlvd2xo4i",
+              favIndex: 0,
+              createdAt: 1619467659,
+              updatedAt: 1619467765,
+              trashed: false,
+              categoryUuid: "107",
+              details: {
+                loginFields: [],
+                notesPlain: "My Reward Card",
+                sections: [
+                  {
+                    title: "",
+                    fields: [
+                      {
+                        title: "company name",
+                        id: "company_name",
+                        value: {
+                          string: "Super Cool Store Co.",
+                        },
+                        indexAtSource: 0,
+                        guarded: false,
+                        multiline: false,
+                        dontGenerate: false,
+                        inputTraits: {
+                          keyboard: "default",
+                          correction: "default",
+                          capitalization: "words",
+                        },
+                      },
+                      {
+                        title: "member name",
+                        id: "member_name",
+                        value: {
+                          string: "Chef Coldroom",
+                        },
+                        indexAtSource: 1,
+                        guarded: false,
+                        multiline: false,
+                        dontGenerate: false,
+                        inputTraits: {
+                          keyboard: "default",
+                          correction: "default",
+                          capitalization: "words",
+                        },
+                      },
+                      {
+                        title: "member ID",
+                        id: "membership_no",
+                        value: {
+                          string: "member-29813569",
+                        },
+                        indexAtSource: 2,
+                        guarded: false,
+                        clipboardFilter:
+                          "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+                        multiline: false,
+                        dontGenerate: false,
+                        inputTraits: {
+                          keyboard: "default",
+                          correction: "no",
+                          capitalization: "none",
+                        },
+                      },
+                      {
+                        title: "PIN",
+                        id: "pin",
+                        value: {
+                          concealed: "99913",
+                        },
+                        indexAtSource: 3,
+                        guarded: false,
+                        multiline: false,
+                        dontGenerate: false,
+                        inputTraits: {
+                          keyboard: "numberPad",
+                          correction: "default",
+                          capitalization: "default",
+                        },
+                      },
+                    ],
+                  },
+                  {
+                    title: "More Information",
+                    name: "extra",
+                    fields: [
+                      {
+                        title: "member ID (additional)",
+                        id: "additional_no",
+                        value: {
+                          string: "additional member id",
+                        },
+                        indexAtSource: 0,
+                        guarded: false,
+                        multiline: false,
+                        dontGenerate: false,
+                        inputTraits: {
+                          keyboard: "default",
+                          correction: "no",
+                          capitalization: "none",
+                        },
+                      },
+                      {
+                        title: "member since",
+                        id: "member_since",
+                        value: {
+                          monthYear: 202101,
+                        },
+                        indexAtSource: 1,
+                        guarded: false,
+                        multiline: false,
+                        dontGenerate: false,
+                        inputTraits: {
+                          keyboard: "default",
+                          correction: "default",
+                          capitalization: "default",
+                        },
+                      },
+                      {
+                        title: "customer service phone",
+                        id: "customer_service_phone",
+                        value: {
+                          phone: "123456",
+                        },
+                        indexAtSource: 2,
+                        guarded: false,
+                        multiline: false,
+                        dontGenerate: false,
+                        inputTraits: {
+                          keyboard: "namePhonePad",
+                          correction: "default",
+                          capitalization: "default",
+                        },
+                      },
+                      {
+                        title: "phone for reservations",
+                        id: "reservations_phone",
+                        value: {
+                          phone: "123456",
+                        },
+                        indexAtSource: 3,
+                        guarded: false,
+                        multiline: false,
+                        dontGenerate: false,
+                        inputTraits: {
+                          keyboard: "namePhonePad",
+                          correction: "default",
+                          capitalization: "default",
+                        },
+                      },
+                      {
+                        title: "website",
+                        id: "website",
+                        value: {
+                          url: "supercoolstore.com",
+                        },
+                        indexAtSource: 4,
+                        guarded: false,
+                        multiline: false,
+                        dontGenerate: false,
+                        inputTraits: {
+                          keyboard: "default",
+                          correction: "default",
+                          capitalization: "default",
+                        },
+                      },
+                    ],
+                  },
+                ],
+                passwordHistory: [],
+              },
+              overview: {
+                subtitle: "Super Cool Store Co.",
+                title: "Retail Reward Thing",
+                url: "",
+                ps: 0,
+                pbe: 0.0,
+                pgrng: false,
+              },
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};

simon04/clients

libs/importers/spec/importers/onepassword-win-csv-importer.spec.ts
@@ -1,0 +1,87 @@
+import { CipherType } from "@bitwarden/common/enums/cipherType";
+import { FieldType } from "@bitwarden/common/enums/fieldType";
+import { CipherView } from "@bitwarden/common/models/view/cipher.view";
+import { FieldView } from "@bitwarden/common/models/view/field.view";
+import { OnePasswordWinCsvImporter as Importer } from "@bitwarden/importers/importers/onepassword/onepassword-win-csv-importer";
+
+import { data as creditCardData } from "./test-data/onepassword-csv/credit-card.windows.csv";
+import { data as identityData } from "./test-data/onepassword-csv/identity.windows.csv";
+import { data as multiTypeData } from "./test-data/onepassword-csv/multiple-items.windows.csv";
+
+function expectIdentity(cipher: CipherView) {
+  expect(cipher.type).toBe(CipherType.Identity);
+
+  expect(cipher.identity).toEqual(
+    expect.objectContaining({
+      firstName: "first name",
+      middleName: "mi",
+      lastName: "last name",
+      username: "userNam3",
+      company: "bitwarden",
+      phone: "8005555555",
+      email: "email@bitwarden.com",
+    })
+  );
+
+  expect(cipher.fields).toEqual(
+    expect.arrayContaining([
+      Object.assign(new FieldView(), {
+        type: FieldType.Text,
+        name: "address",
+        value: "address city state zip us",
+      }),
+    ])
+  );
+}
+
+function expectCreditCard(cipher: CipherView) {
+  expect(cipher.type).toBe(CipherType.Card);
+
+  expect(cipher.card).toEqual(
+    expect.objectContaining({
+      number: "4111111111111111",
+      code: "111",
+      cardholderName: "test",
+      expMonth: "1",
+      expYear: "1970",
+    })
+  );
+}
+
+describe("1Password windows CSV Importer", () => {
+  let importer: Importer;
+  beforeEach(() => {
+    importer = new Importer();
+  });
+
+  it("should parse identity records", async () => {
+    const result = await importer.parse(identityData);
+
+    expect(result).not.toBeNull();
+    expect(result.success).toBe(true);
+    expect(result.ciphers.length).toBe(1);
+    const cipher = result.ciphers[0];
+    expectIdentity(cipher);
+  });
+
+  it("should parse credit card records", async () => {
+    const result = await importer.parse(creditCardData);
+
+    expect(result).not.toBeNull();
+    expect(result.success).toBe(true);
+    expect(result.ciphers.length).toBe(1);
+    const cipher = result.ciphers[0];
+    expectCreditCard(cipher);
+  });
+
+  it("should parse csv's with multiple record types", async () => {
+    const result = await importer.parse(multiTypeData);
+
+    expect(result).not.toBeNull();
+    expect(result.success).toBe(true);
+    expect(result.ciphers.length).toBe(4);
+
+    expectIdentity(result.ciphers[1]);
+    expectCreditCard(result.ciphers[2]);
+  });
+});

libs/importers/spec/importers/test-data/onepassword-1pux/email-field-on-identity_prefilled.ts
@@ -1,0 +1,103 @@
+import { ExportData } from "@bitwarden/importers/importers/onepassword/types/onepassword-1pux-importer-types";
+
+export const EmailFieldOnIdentityPrefilledData: ExportData = {
+  accounts: [
+    {
+      attrs: {
+        accountName: "1Password Customer",
+        name: "1Password Customer",
+        avatar: "",
+        email: "username123123123@gmail.com",
+        uuid: "TRIZ3XV4JJFRXJ3BARILLTUA6E",
+        domain: "https://my.1password.com/",
+      },
+      vaults: [
+        {
+          attrs: {
+            uuid: "pqcgbqjxr4tng2hsqt5ffrgwju",
+            desc: "Just test entries",
+            avatar: "ke7i5rxnjrh3tj6uesstcosspu.png",
+            name: "T's Test Vault",
+            type: "U",
+          },
+          items: [
+            {
+              uuid: "45mjttbbq3owgij2uis55pfrlq",
+              favIndex: 0,
+              createdAt: 1619465450,
+              updatedAt: 1619465789,
+              trashed: false,
+              categoryUuid: "004",
+              details: {
+                loginFields: [],
+                notesPlain: "",
+                sections: [
+                  {
+                    title: "Identification",
+                    name: "name",
+                    fields: [],
+                  },
+                  {
+                    title: "Address",
+                    name: "address",
+                    fields: [],
+                  },
+                  {
+                    title: "Internet Details",
+                    name: "internet",
+                    fields: [
+                      {
+                        title: "email",
+                        id: "email",
+                        value: {
+                          string: "gengels@nullvalue.test",
+                        },
+                        indexAtSource: 4,
+                        guarded: false,
+                        multiline: false,
+                        dontGenerate: false,
+                        inputTraits: {
+                          keyboard: "emailAddress",
+                          correction: "default",
+                          capitalization: "default",
+                        },
+                      },
+                      {
+                        title: "2nd email",
+                        id: "2nd_email",
+                        value: {
+                          email: {
+                            email_address: "kriddler@nullvalue.test",
+                            provider: "myEmailProvider",
+                          },
+                        },
+                        indexAtSource: 1,
+                        guarded: false,
+                        multiline: false,
+                        dontGenerate: false,
+                        inputTraits: {
+                          keyboard: "emailAddress",
+                          correction: "default",
+                          capitalization: "default",
+                        },
+                      },
+                    ],
+                  },
+                ],
+                passwordHistory: [],
+              },
+              overview: {
+                subtitle: "George Engels",
+                title: "George Engels",
+                url: "",
+                ps: 0,
+                pbe: 0.0,
+                pgrng: false,
+              },
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};

libs/importers/spec/importers/test-data/onepassword-1pux/rewards-program.ts
@@ -1,0 +1,209 @@
+import { ExportData } from "@bitwarden/importers/importers/onepassword/types/onepassword-1pux-importer-types";
+
+export const RewardsProgramData: ExportData = {
+  accounts: [
+    {
+      attrs: {
+        accountName: "1Password Customer",
+        name: "1Password Customer",
+        avatar: "",
+        email: "username123123123@gmail.com",
+        uuid: "TRIZ3XV4JJFRXJ3BARILLTUA6E",
+        domain: "https://my.1password.com/",
+      },
+      vaults: [
+        {
+          attrs: {
+            uuid: "pqcgbqjxr4tng2hsqt5ffrgwju",
+            desc: "Just test entries",
+            avatar: "ke7i5rxnjrh3tj6uesstcosspu.png",
+            name: "T's Test Vault",
+            type: "U",
+          },
+          items: [
+            {
+              uuid: "3bmrdcml3tngvsr6zdlvd2xo4i",
+              favIndex: 0,
+              createdAt: 1619467659,
+              updatedAt: 1619467765,
+              trashed: false,
+              categoryUuid: "107",
+              details: {
+                loginFields: [],
+                notesPlain: "My Reward Card",
+                sections: [
+                  {
+                    title: "",
+                    fields: [
+                      {
+                        title: "company name",
+                        id: "company_name",
+                        value: {
+                          string: "Super Cool Store Co.",
+                        },
+                        indexAtSource: 0,
+                        guarded: false,
+                        multiline: false,
+                        dontGenerate: false,
+                        inputTraits: {
+                          keyboard: "default",
+                          correction: "default",
+                          capitalization: "words",
+                        },
+                      },
+                      {
+                        title: "member name",
+                        id: "member_name",
+                        value: {
+                          string: "Chef Coldroom",
+                        },
+                        indexAtSource: 1,
+                        guarded: false,
+                        multiline: false,
+                        dontGenerate: false,
+                        inputTraits: {
+                          keyboard: "default",
+                          correction: "default",
+                          capitalization: "words",
+                        },
+                      },
+                      {
+                        title: "member ID",
+                        id: "membership_no",
+                        value: {
+                          string: "member-29813569",
+                        },
+                        indexAtSource: 2,
+                        guarded: false,
+                        clipboardFilter:
+                          "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+                        multiline: false,
+                        dontGenerate: false,
+                        inputTraits: {
+                          keyboard: "default",
+                          correction: "no",
+                          capitalization: "none",
+                        },
+                      },
+                      {
+                        title: "PIN",
+                        id: "pin",
+                        value: {
+                          concealed: "99913",
+                        },
+                        indexAtSource: 3,
+                        guarded: false,
+                        multiline: false,
+                        dontGenerate: false,
+                        inputTraits: {
+                          keyboard: "numberPad",
+                          correction: "default",
+                          capitalization: "default",
+                        },
+                      },
+                    ],
+                  },
+                  {
+                    title: "More Information",
+                    name: "extra",
+                    fields: [
+                      {
+                        title: "member ID (additional)",
+                        id: "additional_no",
+                        value: {
+                          string: "additional member id",
+                        },
+                        indexAtSource: 0,
+                        guarded: false,
+                        multiline: false,
+                        dontGenerate: false,
+                        inputTraits: {
+                          keyboard: "default",
+                          correction: "no",
+                          capitalization: "none",
+                        },
+                      },
+                      {
+                        title: "member since",
+                        id: "member_since",
+                        value: {
+                          monthYear: 202101,
+                        },
+                        indexAtSource: 1,
+                        guarded: false,
+                        multiline: false,
+                        dontGenerate: false,
+                        inputTraits: {
+                          keyboard: "default",
+                          correction: "default",
+                          capitalization: "default",
+                        },
+                      },
+                      {
+                        title: "customer service phone",
+                        id: "customer_service_phone",
+                        value: {
+                          phone: "123456",
+                        },
+                        indexAtSource: 2,
+                        guarded: false,
+                        multiline: false,
+                        dontGenerate: false,
+                        inputTraits: {
+                          keyboard: "namePhonePad",
+                          correction: "default",
+                          capitalization: "default",
+                        },
+                      },
+                      {
+                        title: "phone for reservations",
+                        id: "reservations_phone",
+                        value: {
+                          phone: "123456",
+                        },
+                        indexAtSource: 3,
+                        guarded: false,
+                        multiline: false,
+                        dontGenerate: false,
+                        inputTraits: {
+                          keyboard: "namePhonePad",
+                          correction: "default",
+                          capitalization: "default",
+                        },
+                      },
+                      {
+                        title: "website",
+                        id: "website",
+                        value: {
+                          url: "supercoolstore.com",
+                        },
+                        indexAtSource: 4,
+                        guarded: false,
+                        multiline: false,
+                        dontGenerate: false,
+                        inputTraits: {
+                          keyboard: "default",
+                          correction: "default",
+                          capitalization: "default",
+                        },
+                      },
+                    ],
+                  },
+                ],
+                passwordHistory: [],
+              },
+              overview: {
+                subtitle: "Super Cool Store Co.",
+                title: "Retail Reward Thing",
+                url: "",
+                ps: 0,
+                pbe: 0.0,
+                pgrng: false,
+              },
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
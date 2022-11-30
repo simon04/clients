cd9b1a35ab6e04256666dbf12a7f06a3fdceb4cd
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2022-11-30 13:59:43 -0500

    Update apps/browser/src/background/main.background.ts
    Co-authored-by: Matt Gibson <mgibson@bitwarden.com>

--- apps/browser/src/background/main.background.ts
+++ apps/browser/src/background/main.background.ts
@@ -584,7 +584,7 @@ export default class MainBackground {
 
     await this.tabsBackground.init();
     if (!this.popupOnlyContext) {
-      this.contextMenusBackground.init();
+      this.contextMenusBackground?.init();
     }
     await this.idleBackground.init();
     await this.webRequestBackground.init();

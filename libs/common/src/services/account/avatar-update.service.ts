fc45270e61e3454133c55cba0e1a16d122f76fd3
Brandon Maharaj <bmaharaj@bitwarden.com>
2022-12-30 14:00:50 -0500

    [SG-926] [SG-58] [Defect] - Selected Avatar color does not persist in the Account Settings menu (#4359)
    * SG-926
    
    * fix: comment
    
    * fix: undo
    
    * fix: imp

--- libs/common/src/services/account/avatar-update.service.ts
+++ libs/common/src/services/account/avatar-update.service.ts
@@ -1,22 +1,24 @@
 import { BehaviorSubject, Observable } from "rxjs";
 
+import { AvatarUpdateService as AvatarUpdateServiceAbstraction } from "../../abstractions/account/avatar-update.service";
 import { ApiService } from "../../abstractions/api.service";
 import { StateService } from "../../abstractions/state.service";
 import { UpdateAvatarRequest } from "../../models/request/update-avatar.request";
 import { ProfileResponse } from "../../models/response/profile.response";
 
-export class AvatarUpdateService {
+export class AvatarUpdateService implements AvatarUpdateServiceAbstraction {
   private _avatarUpdate$ = new BehaviorSubject<string | null>(null);
   avatarUpdate$: Observable<string | null> = this._avatarUpdate$.asObservable();
 
   constructor(private apiService: ApiService, private stateService: StateService) {
-    this.loadColorFromState().then((color) => {
-      this._avatarUpdate$.next(color);
-    });
+    this.loadColorFromState();
   }
 
   loadColorFromState(): Promise<string | null> {
-    return this.stateService.getAvatarColor();
+    return this.stateService.getAvatarColor().then((color) => {
+      this._avatarUpdate$.next(color);
+      return color;
+    });
   }
 
   pushUpdate(color: string | null): Promise<ProfileResponse | void> {

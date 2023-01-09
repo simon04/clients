829220a0a2af97a533ff94cd41cf80ffd850b41e
Colton Hurst <colton@coltonhurst.com>
2023-01-09 17:15:18 -0500

    Add password verification

--- bitwarden_license/bit-web/src/app/secrets-manager/settings/export/sm-export.component.ts
+++ bitwarden_license/bit-web/src/app/secrets-manager/settings/export/sm-export.component.ts
@@ -5,6 +5,9 @@ import { Subject, switchMap, takeUntil } from "rxjs";
 
 import { I18nService } from "@bitwarden/common/abstractions/i18n.service";
 import { OrganizationService } from "@bitwarden/common/abstractions/organization/organization.service.abstraction";
+import { PlatformUtilsService } from "@bitwarden/common/abstractions/platformUtils.service";
+import { UserVerificationService } from "@bitwarden/common/abstractions/userVerification/userVerification.service.abstraction";
+import { VerificationType } from "@bitwarden/common/enums/verificationType";
 
 import { SMSettingsService } from "../sm-settings.service";
 
@@ -14,16 +17,22 @@ import { SMSettingsService } from "../sm-settings.service";
 })
 export class SMExportComponent implements OnInit, OnDestroy {
   private destroy$ = new Subject<void>();
+
   protected orgName: string;
+  protected exportFormats: string[] = ["json"];
+
   protected formGroup = new FormGroup({
-    format: new FormControl("", [Validators.required]),
+    format: new FormControl("json", [Validators.required]),
+    masterPassword: new FormControl("", [Validators.required]),
   });
 
   constructor(
     private route: ActivatedRoute,
     private i18nService: I18nService,
     private settingsService: SMSettingsService,
-    private organizationService: OrganizationService
+    private organizationService: OrganizationService,
+    private userVerificationService: UserVerificationService,
+    private platformUtilsService: PlatformUtilsService
   ) {}
 
   async ngOnInit() {
@@ -35,6 +44,8 @@ export class SMExportComponent implements OnInit, OnDestroy {
       .subscribe((organization) => {
         this.orgName = organization.name;
       });
+
+    this.formGroup.get("format").disable();
   }
 
   async ngOnDestroy() {
@@ -42,13 +53,29 @@ export class SMExportComponent implements OnInit, OnDestroy {
     this.destroy$.complete();
   }
 
-  async submit() {
+  submit = async () => {
     this.formGroup.markAllAsTouched();
 
     if (this.formGroup.invalid) {
       return;
     }
 
+    try {
+      // Incorrect secret will throw an invalid password error.
+      await this.userVerificationService.verifyUser({
+        type: VerificationType.MasterPassword,
+        secret: this.formGroup.get("masterPassword").value,
+      });
+    } catch (e) {
+      this.platformUtilsService.showToast(
+        "error",
+        this.i18nService.t("error"),
+        this.i18nService.t("invalidMasterPassword")
+      );
+      return;
+    }
+
+    // Do API call, user is verified
     alert("submitted");
-  }
+  };
 }
